68a623116bb844e83b86c5fc47f4fdfb60f59805
ryroshan <roshanros13@gmail.com>
2021-04-09 15:49:08 +0530

    adding

---
+++ README.md
@@ -0,0 +1,23 @@
+## Spacex
+A front-end application which would help users list and browse all launches by SpaceX program.
+
+## Installation
+To install, simply clone this repository & run the following command  `yarn` or `npm install`, this will install all the dependencies for you.
+
+## dependencies
+react-dom
+react-bootstrap
+bootstrap
+
+
+## Running Project
+To run this project locally, run the following command  `yarn start` or `npm start`, this will start a local instance of the application & you will be able to run the app in your browser at this url http://localhost:3000/.
+
+### Heroku Deployment
+
+SpaceX app is deployed on **Heruko** [Spacex-Launch-Programs](deployed url)
+
+
+
+
+Developed By [ Roshan Yadav ](https://github.com/ryroshan)
---
+++ src/App.js
@@ -0,0 +1,229 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Card, Button, Row, Col } from 'react-bootstrap';
+import RocketLaunchDetails from './RocketLaunchDetails';
+import querystring from 'querystring';
+import './App.css';
+import loader from './loadRocket.gif';
+
+const API_BASE_URL = "https://api.spacexdata.com/v3/launches?limit=100";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+      filters: {
+        limit: 150,
+        launch_year: undefined,
+        launch_success: undefined,
+        land_success: undefined,
+      },
+    }
+
+  }
+
+  getUpdatedApiUrl(filters = {}) {
+    return API_BASE_URL + querystring.stringify({ ...filters });
+  }
+
+  fetchAPI(filters) {
+    const URL = this.getUpdatedApiUrl(filters);
+    this.setState({ isLoaded: false, filters });
+    fetch(URL)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          isLoaded: true,
+          data
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.fetchAPI(this.state.filters);
+  }
+
+  updateApiFilters(type, value) {
+    // if same value is clicked, we remove that filter
+    if (this.state.filters[type] === value) {
+      value = undefined;
+    }
+
+    const filters = {
+      ...this.state.filters,
+      [type]: value,
+    };
+
+    this.fetchAPI(filters);
+  }
+
+
+  render() {
+
+    const { isLoaded, data } = this.state;
+    const uniqueLaunchYears = new Array(16).fill(0).map((_, index) => 2006 + index);
+
+    if (!isLoaded) {
+      return <div className="App-loader-container">
+        <div className="App-loader-box">
+          <img src={loader} alt="loading..." />
+        </div>
+      </div>
+    }
+
+    else {
+
+      return (
+        <div className="App">
+          <h1 className="App-header">SpaceX Launch Programs</h1>
+          <Container fluid>
+            <Row>
+              <Col xs={12} sm={12} md={6} lg={3}>
+                <Card className="App-filter-card">
+                  <Card.Body>
+                    <Card.Title className="App-filter-header">
+                      Filters
+                    </Card.Title>
+                    <Card.Text className="App-filter-heading-launch-year">
+                      Launch Year
+                      <hr className="App-filters-hr" />
+                    </Card.Text>
+
+                    <Row>
+                      <div className="App-filter-button-container">
+                        {uniqueLaunchYears.map((year) => {
+                          return (
+                            <Button
+                              className="App-filter-button"
+                              variant={
+                                this.state.filters.launch_year ===
+                                year.toString()
+                                  ? "success"
+                                  : "outline-success"
+                              }
+                              value={year}
+                              onClick={(e) =>
+                                this.updateApiFilters(
+                                  "launch_year",
+                                  e.target.value
+                                )
+                              }
+                            >
+                              {year}
+                            </Button>
+                          );
+                        })}
+                      </div>
+                    </Row>
+
+                    <Card.Text className="App-filter-heading">
+                      Successful Launch
+                      <hr className="App-filters-hr" />
+                    </Card.Text>
+
+                    <div className="App-filter-button-container">
+                      <Button
+                        className="App-filter-button"
+                        variant={
+                          this.state.filters.launch_success === "true"
+                            ? "success"
+                            : "outline-success"
+                        }
+                        onClick={(e) =>
+                          this.updateApiFilters(
+                            "launch_success",
+                            e.target.value
+                          )
+                        }
+                        value="true"
+                      >
+                        True
+                      </Button>
+
+                      <Button
+                        className="App-filter-button"
+                        variant={
+                          this.state.filters.launch_success === "false"
+                            ? "success"
+                            : "outline-success"
+                        }
+                        onClick={(e) =>
+                          this.updateApiFilters(
+                            "launch_success",
+                            e.target.value
+                          )
+                        }
+                        value="false"
+                      >
+                        False
+                      </Button>
+                    </div>
+
+                    <Card.Text className="App-filter-heading">
+                      Successful Landing
+                      <hr className="App-filters-hr" />
+                    </Card.Text>
+                    <div className="App-filter-button-container">
+                      <Button
+                        className="App-filter-button"
+                        variant={
+                          this.state.filters.land_success === "true"
+                            ? "success"
+                            : "outline-success"
+                        }
+                        onClick={(e) =>
+                          this.updateApiFilters("land_success", e.target.value)
+                        }
+                        value="true"
+                      >
+                        True
+                      </Button>
+
+                      <Button
+                        className="App-filter-button"
+                        variant={
+                          this.state.filters.land_success === "false"
+                            ? "success"
+                            : "outline-success"
+                        }
+                        onClick={(e) =>
+                          this.updateApiFilters("land_success", e.target.value)
+                        }
+                        value="false"
+                      >
+                        False
+                      </Button>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </Col>
+
+              <Col xs={12} sm={12} md={6} lg={9}>
+                <Row>
+                  {data.map((details) => {
+                    return (
+                      <Col md={12} lg={4}>
+                        <RocketLaunchDetails details={details} />
+                      </Col>
+                    );
+                  })}
+                </Row>
+              </Col>
+            </Row>
+            <div>
+              <h5 className="App-Developers-name">
+                Developed by : Roshan Yadav 
+              </h5>
+            </div>
+          </Container>
+        </div>
+      );
+    }
+
+  }
+}
+
+export default App;
